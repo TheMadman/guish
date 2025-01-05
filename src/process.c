6dae6cd2464978c30918e71b6463599d304c9d45
Marcus Harrison <marcus@harrisonland.co.uk>
2025-01-05 16:58:48 +0100

    Implement setting up socket

--- src/process.c
+++ src/process.c
@@ -3,9 +3,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <wayland-client-core.h>
-
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <string.h>
 #include <stdio.h>
+#include <poll.h>
+#include <libadt/util.h>
+
 #include <sys/wait.h>
+
+#define arrlength libadt_util_arrlength
+#define arrend libadt_util_arrend
+
+typedef struct {
+	int client, server, error;
+} sockpair_t;
+
+typedef char buf_t[4096];
+
+static sockpair_t create_socketpair(int domain, int type, int protocol)
+{
+	int sockpair[2] = { 0 };
+	int error = socketpair(domain, type, protocol, sockpair) < 0;
+	return (sockpair_t) {
+		.client = sockpair[0],
+		.server = sockpair[1],
+		.error = error,
+	};
+}
 
 int fork_wrapper(
 	struct parse_statement_command command,
@@ -13,17 +38,31 @@ int fork_wrapper(
 	int guiout
 )
 {
+	sockpair_t sockets = create_socketpair(AF_UNIX, SOCK_STREAM, 0);
+	if (sockets.error)
+		return -1;
+
 	int pid = fork();
 	switch (pid) {
 		case -1:
-			perror("fork");
 			return -1;
-		case 0:
-			// make command run with custom wayland thingy somehow
+		case 0: {
+			close(sockets.server);
+			char fdbuf[11] = { 0 };
+			snprintf(fdbuf, sizeof(fdbuf), "%d", sockets.client);
+			setenv("WAYLAND_SOCKET", fdbuf, 1);
 			return exec_command(command);
-		default:
+		}
+		default: {
+			close(sockets.client);
+			char buf[4096] = { 0 };
+			ssize_t amount = read(sockets.server, buf, sizeof(buf));
+			printf("client sent %ld bytes\n", amount);
+			kill(pid, SIGTERM);
 			// set up a client socket to the parent
 			wait(NULL);
+			return 0;
+		}
 	}
 }
 
@@ -39,8 +78,9 @@ int exec_command(struct parse_statement_command command)
 	) {
 		const struct libadt_const_lptr
 			*current = libadt_const_lptr_raw(command.statement);
-		*out = libadt_const_lptr_raw(*current);
+		*out = strndup(libadt_const_lptr_raw(*current), (size_t)current->length);
 	}
 
+	// I don't care if we leak memory if execvp() fails
 	return execvp(*args, args);
 }
